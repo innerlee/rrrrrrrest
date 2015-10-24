a3302d4e418f6b9d20320b0fcae1389d393a5271
innerlee <363664470@qq.com>
2015-10-24 23:38:46 +0800

    init co.

---
+++ rrrrrrrest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace rrrrrrrest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient();
+            client.BaseUrl = new Uri("http://tagger.chinacloudsites.cn");
+            //client.Authenticator = new HttpBasicAuthenticator("username", "password");
+
+            var request = new RestRequest();
+            request.Method = Method.POST;
+            request.Resource = "SwipeCard/Check";
+
+            IRestResponse response = client.Execute(request);
+            Console.Write(response.Content == "1" ? "open" : "closed");
+        }
+    }
+}
+
